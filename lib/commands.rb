4000e53f531275fde3bb1dd529c0af95696578cc
Toon Willems <willemstoon@gmail.com>
2010-07-26 20:07:51 +0200

    added error and notice commands

--- lib/commands.rb
+++ lib/commands.rb
@@ -1,4 +1,4 @@
-require 'command'
+require_relative 'command'
 # the NICK command
 Command.create 'nick' do
   parse ":nickname"
@@ -11,4 +11,14 @@ Command.create 'user' do
   param :username, :hostname, :servername, :realname do
     default "*"
   end
+end
+
+Command.create 'notice' do
+  parse ':nickname : :text'
+  param :nickname, :text
+end
+
+Command.create 'error' do
+  parse ': :message'
+  param :message
 end
