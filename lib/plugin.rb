1bcb4fa96e48cb54cd83e5a9d4d30b2473211cfa
Toon Willems <willemstoon@gmail.com>
2010-07-28 16:39:35 +0200

    cleaned up reload, convenience dsl for responding to commands

--- lib/plugin.rb
+++ lib/plugin.rb
@@ -6,15 +6,16 @@ class Plugin
     plugins << new_plugin.new
   end
 
+  class << self
+    attr_accessor :plugin_dir
+  end
+
   def self.reload
-    # i hate the joinplugin, it's really ugly, but i really have to remove it
-    # here
-    @plugins.delete_if {|plu| plu.class == JoinPlugin} 
     @plugins.each do |plu|
-      Object.class_eval {remove_const plu.class.name.to_sym}
+      Object.class_eval {remove_const plu.class.name.to_sym} rescue nil
     end
     @plugins = []
-    load_plugins @plugin_dir
+    load_plugins
   end
 
   def self.plugins
@@ -25,14 +26,47 @@ class Plugin
     plugins.dup.each &b
   end
 
-  def self.load_plugins(dir_name = 'plugins')
-    @plugin_dir = dir_name
-    Dir["#{dir_name}/*.rb"].each {|plugin| load plugin}
+  def self.load_plugins
+    Dir["#{plugin_dir}/*.rb"].each {|plugin| load plugin}
   end
 
   def on(name, command)
     send("on_#{name.downcase}", command)
   end 
+  
+  # store the block for later
+  # the block can take 0, 1, 2 or 3 arguments
+  # |query, response, sender| arguments will be left out from right to left
+  # call this method in the initialize method
+  def self.on_command(command, &block)
+    commands[command] = block
+  end
+  
+  def self.commands
+    @commands ||= {}
+  end
+
+  def respond(command)
+    c = Command::PRIVMSG.new
+    c.receiver = command.receiver
+    c
+  end
+ 
+  # do some filtering in the baseclass
+  #
+  def on_privmsg(command)
+    responses = []
+    self.class.commands.each do |com,block|
+      m = command.message.match(/^#{com}(.*)$/)
+      resp = respond(command) 
+      query = m[1].strip rescue nil 
+
+      call_args = [query, resp, command.sender]
+      response = block.call *call_args[0..block.arity] if m
+      responses << response                        
+    end
+    responses.compact
+  end
 
   def method_missing(name, *args)
     super unless name =~ /^on_(\w*)$/
