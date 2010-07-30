d905cf1650fdb92ff278f1078582dc7beff9d8d2
Toon Willems <willemstoon@gmail.com>
2010-07-30 13:59:02 +0200

    tell_plugin now persists correctly to a db

--- plugins/tell_plugin.rb
+++ plugins/tell_plugin.rb
@@ -1,28 +1,37 @@
 class TellPlugin < Plugin
 
-  on_command '!tell' do |query, response|
+  on_command '!tell' do |query, response, sender|
     query = query.split
     to = query.delete_at 0
-    tells[to] << to + ': ' + Time.now.strftime("%H:%M") + "<#{sender}>" + query.join(' ')
+    self.add_tell(to, to + ': ' + Time.now.strftime("%H:%M") + "<#{sender}> " + query.join(' '))
     response.message = "I'll pass that on when #{to} is around."
   end
 
   def on_privmsg(command)
     super_result = super
-    tells = self.class.tells[command.sender]
-    unless tells.empty?
+    tells = self.class.tells(command.sender)
+    if tells
       c = respond(command) 
       tells.map! do |message|
         c.dup.tap {|com| com.message = message}
       end
-      self.class.tells[command.sender] = []
+      self.class.clear_tells command.sender
+    else
+      tells = []
     end
     (tells + super_result).flatten.compact
   end
 
-  def self.tells
-    store['tells'] = Hash.new {|h,k| h[k] = [] } unless store['tells']
-    store['tells']
+  def self.add_tell(to, message)
+    self.store.sadd "tells_#{to}", message
+  end
+  
+  def self.clear_tells(user)
+    self.store.del "tells_#{user}"
+  end
+
+  def self.tells(user)
+    self.store.smembers "tells_#{user}"
   end
 
 end
