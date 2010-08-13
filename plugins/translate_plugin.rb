e4211a37cddc6f13fab7b9d04aad9042159174a0
Toon Willems <willemstoon@gmail.com>
2010-08-13 18:32:58 +0200

    added translate plugin

---
+++ plugins/translate_plugin.rb
@@ -0,0 +1,16 @@
+require 'net/http'
+require 'uri'
+require 'cgi'
+require 'json'
+
+class TranslatePlugin < Plugin
+
+  on_command '!translate' do |query, command|
+    query = CGI.escape(query)
+    uri = URI.parse 'http://ajax.googleapis.com/'
+    request_path = "/ajax/services/language/translate?v=1.0&q=#{query}&langpair=%7Cnl"
+    resp = JSON[Net::HTTP.get(uri.host, request_path)]
+    command.message = resp['responseData']['translatedText']
+  end
+
+end
