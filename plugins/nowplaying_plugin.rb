604c05e6140b67de4511f5c7467d0620f26ffda0
Toon Willems <willemstoon@gmail.com>
2010-08-21 12:01:52 +0200

    better xpath expression

--- plugins/nowplaying_plugin.rb
+++ plugins/nowplaying_plugin.rb
@@ -6,7 +6,7 @@ class NowPlayingPlugin < Plugin
   on_command '!nowplaying' do |query, response, sender|
     doc = Net::HTTP.get URI.parse("http://internetradio.vrt.be/internetradio_master/productiesysteem2/song_noa/noa_41.xml")
     xml = REXML::Document.new doc        
-    info = REXML::XPath.each(xml, "//titlename | //artistname").map(&:text)
+    info = REXML::XPath.each(xml, "//item[@index=0]//titlename | //item[@index=0]//artistname").map(&:text)
     response.message = "#{sender}: #{info[1]} - #{info[3]}" 
   end
 
