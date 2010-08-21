dbc5487c01ff71e58e16c43115ce3d8b2dc0c0cb
Toon Willems <willemstoon@gmail.com>
2010-08-21 11:47:12 +0200

    added nowplaying plugin

---
+++ plugins/nowplaying_plugin.rb
@@ -0,0 +1,13 @@
+require 'rexml/document'
+require 'net/http'
+
+class NowPlayingPlugin < Plugin
+  
+  on_command '!nowplaying' do |query, response, sender|
+    doc = Net::HTTP.get URI.parse("http://internetradio.vrt.be/internetradio_master/productiesysteem2/song_noa/noa_41.xml")
+    xml = REXML::Document.new doc        
+    info = REXML::XPath.each(xml, "//titlename | //artistname").map(&:text)
+    response.message = "#{sender}: #{info[1]} - #{info[3]}" 
+  end
+
+end
