1f7e27ae6ab461bbd5f4f00f447e02a00c93a7f5
Denis Defreyne <denis.defreyne@stoneship.org>
2010-08-21 12:09:08 +0200

    replaced nowplaying plugin with a vastly superior version

--- plugins/nowplaying_plugin.rb
+++ plugins/nowplaying_plugin.rb
@@ -1,13 +1,19 @@
-require 'rexml/document'
-require 'net/http'
+require 'nokogiri'
+require 'open-uri'
 
 class NowPlayingPlugin < Plugin
-  
-  on_command '!nowplaying' do |query, response, sender|
-    doc = Net::HTTP.get URI.parse("http://internetradio.vrt.be/internetradio_master/productiesysteem2/song_noa/noa_41.xml")
-    xml = REXML::Document.new doc        
-    info = REXML::XPath.each(xml, "//item[@index=0]//titlename | //item[@index=0]//artistname").map(&:text)
-    response.message = "#{sender}: #{info[1]} - #{info[3]}" 
+ 
+  on_command '!nowplaying' do |query, response|
+    case query
+      when 'stubru'
+        doc = Nokogiri::XML(open('http://internetradio.vrt.be/internetradio_master/productiesysteem2/song_noa/noa_41.xml'))
+        item = doc.xpath('//item[@index=0]').first
+        title  = item.xpath('title/titlename').first.inner_text
+        artist = item.xpath('artist/artistname').first.inner_text
+        response.message = artist.empty? ? title : "#{artist} - #{title}"
+      else
+        response.message = "tscheelt dak weet oedak moe opzoeken wa der op “#{query}” aant spelen is maat"
+    end
   end
 
 end
